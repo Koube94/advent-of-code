2d1174836ccfd139b1dbb25843d5a67db544a925
Koube94 <56591276+Koube94@users.noreply.github.com>
2024-12-08 12:02:34 +0100

    Day 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::error::Error;
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let message: String = fs::read_to_string("src/input.txt")?;
+    let mut first_col: Vec<i32> = vec![];
+    let mut second_col: Vec<i32> = vec![];
+    for line in message.lines() {
+        let parts = line.split_whitespace().collect::<Vec<&str>>();
+        first_col.push(parts[0].parse::<i32>().unwrap());
+        second_col.push(parts[1].parse::<i32>().unwrap())
+    }
+    // First task
+    first_col.sort();
+    second_col.sort();
+    let mut distance = 0;
+    for (first, second) in first_col.iter().zip(second_col.iter()){
+        distance += (first - second).abs();
+    }
+    println!("{}", distance);
+    // Second task
+    let mut similarity_score = 0;
+    for first in first_col.iter(){
+        let mut count = 0;
+        for second in second_col.iter(){
+            if first == second {
+                count += 1;
+            }
+        }
+        similarity_score += first * count;
+    }
+    println!("{}", similarity_score);
+    Ok(())
 }
--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,90 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::error::Error;
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Read the file content as a string
+    let message = fs::read_to_string("src/input_short.txt")?;
+
+    // Parse the string into a vector of vectors of integers
+    let reports: Vec<Vec<i32>> = message
+        .lines() // Iterate over lines
+        .map(|line| {
+            line.split_whitespace() // Split the line into words
+                .filter_map(|word| word.parse::<i32>().ok()) // Parse each word into an i32, filter out errors
+                .collect::<Vec<i32>>() // Collect parsed integers into a vector
+        })
+        .collect(); // Collect all lines into a vector of vectors
+    let min_diff = 1;
+    let max_diff = 3;
+    
+    // Task 1
+    let mut valid_reports = 0;
+    for report in reports.iter(){
+        let mut increasing = true;
+        let mut decreasing = true;
+        let mut difference_check = true;
+        let mut last_level = -10;
+        for level in report.iter(){
+            if last_level == -10 {
+                last_level = *level;
+                continue;
+            }
+            if level > &last_level{
+                decreasing = false;
+            }
+            if level < &last_level{
+                increasing = false;
+            }
+            let difference = (last_level - level).abs();
+            if difference >= min_diff && difference <= max_diff {
+            }
+            else{
+                difference_check = false;
+            }
+            last_level = *level;
+        }
+        let level_valid = (increasing || decreasing) && difference_check;
+        if level_valid{
+            valid_reports += 1;
+        }
+    }
+    println!("{}", valid_reports);
+    
+    // Task 2
+
+    let mut valid_reports = 0;
+    for report in reports.iter(){
+        let mut last_valid_index = 0;
+        let mut increasing = true;
+        let mut decreasing = true;
+        let valid_level = true;
+        for (index, level) in report.iter().enumerate(){
+            if index == 0 {
+                continue;
+            }
+            let difference = report[index] - report[last_valid_index];
+            if difference.abs() >= min_diff && difference.abs() <= max_diff{
+            }
+            else {
+                continue;
+            }
+            if difference < 0 {
+                if increasing {
+                    increasing = false;
+                    continue
+                }
+            }
+            if difference > 0 {
+                if decreasing {
+                    decreasing = false;
+                    continue
+                }
+            }
+            last_valid_index = index;
+        }
+    }
+    println!("{}", valid_reports);
+    
+    Ok(())
 }
